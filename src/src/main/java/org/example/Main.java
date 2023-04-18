2a05fe3fc43d370dad08c42fe33c56b9a57eccd2
Samuel_Alvarez <s.alvarez05@ufromail.cl>
2023-04-18 19:31:17 -0400

    Creacion de clase MainTest y cambio de variable float a double

--- src/src/main/java/org/example/Main.java
+++ src/src/main/java/org/example/Main.java
@@ -9,12 +9,12 @@ public class Main {
     }
 
     public static void solucionSistemaEcuaciones() {
-        float A=0;
-        float B=0;
-        float C=0;
-        float D=0;
-        float E=0;
-        float F=0;
+        double A=0;
+        double B=0;
+        double C=0;
+        double D=0;
+        double E=0;
+        double F=0;
         try {
             Scanner teclado = new Scanner(System.in);
             System.out.println("Ingrese el valor de A");
@@ -33,16 +33,16 @@ public class Main {
             System.out.println("ingrese numeros (Si es decimal use comas)");
             System.exit(1);
         }
-        float y =CalculoDeY(A,B,C,D,E,F);
-        float x =CalculoDeX(A,B,C,D,E,F);
+        double y =CalculoDeY(A,B,C,D,E,F);
+        double x =CalculoDeX(A,B,C,D,E,F);
 
 
         System.out.println("El valor de X es: " + x);
         System.out.println("El valor de Y es: " + y);
     }
 
-    private static float CalculoDeX(float A, float B, float C, float D, float E, float F) {
-        float x;
+    public static double CalculoDeX(double A, double B, double C, double D, double E, double F) {
+        double x;
 
         if (B==0&&E==0){
             x=((C+F)/(A+D));
@@ -54,8 +54,8 @@ public class Main {
         return x;
     }
 
-    private static float CalculoDeY(float A, float B, float C, float D, float E, float F) {
-        float y;
+    public static double CalculoDeY(double A, double B, double C, double D, double E, double F) {
+        double y;
         if (A==0 && D==0) {
             y = ((C + F) / (B + E));
         }else if(B==0&&E==0){
---
+++ src/src/test/java/org/example/MainTest.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testCalculoDeX() {
+        double resultadoEsperado1 = 14;
+        assertEquals(Main.CalculoDeX(3,2,6,4,3,2),resultadoEsperado1);
+        double resultadoEsperado2 = -0.5;
+        assertEquals(Main.CalculoDeX(0,2,6,4,3,7),resultadoEsperado2);
+    }
+
+    @Test
+    void testCalculoDeY() {
+        double resultadoEsperado1 = -18;
+        assertEquals(Main.CalculoDeY(3,2,6,4,3,2),resultadoEsperado1);
+        double resultadoEsperado2 = 3;
+        assertEquals(Main.CalculoDeY(0,2,6,4,3,7),resultadoEsperado2);
+    }
+}
