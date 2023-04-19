24f74f8cb5b6dc0189d8138da7cdbe3f6e73b0ca
LightesCube <diegomarillanblooop@gmail.com>
2023-04-18 20:13:30 -0400

    Se sube Codigo parte 5 con try catch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        double x1=0;
+        double x2=0;
+        double y1=0;
+        double y2=0;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Ingrese la coordenada x del primer punto: "); //Siendo X1 y X2 las variables dej eje X para calcular la ecuacion de la recta
-        double x1 = sc.nextDouble();
-        System.out.print("Ingrese la coordenada y del primer punto: ");
-        double y1 = sc.nextDouble();
-        System.out.print("Ingrese la coordenada x del segundo punto: "); //Siendo Y1 y Y2 las variables dej eje Y para calcular la ecuacion de la recta
-        double x2 = sc.nextDouble();
-        System.out.print("Ingrese la coordenada y del segundo punto: ");
-        double y2 = sc.nextDouble();
+        try {
+            System.out.print("Ingrese la coordenada x del primer punto: "); //Siendo X1 y X2 las variables dej eje X para calcular la ecuacion de la recta
+            x1 = sc.nextDouble();
+            System.out.print("Ingrese la coordenada y del primer punto: ");
+            y1 = sc.nextDouble();
+            System.out.print("Ingrese la coordenada x del segundo punto: "); //Siendo Y1 y Y2 las variables dej eje Y para calcular la ecuacion de la recta
+            x2 = sc.nextDouble();
+            System.out.print("Ingrese la coordenada y del segundo punto: ");
+            y2 = sc.nextDouble();
+        }
+        catch(InputMismatchException e){System.out.println("El caracter seleccionado no es valido, por favor intentelo de nuevo");
+            System.exit(1);
+        }
         double b = calcularB(x1, y1, x2, y2);
         double m = calcularM(x1, y1, x2, y2);
         System.out.println("La ecuación de la recta es y = " + b + "x + " + m);
